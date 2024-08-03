0de5a44e7b219198a7c8d1634ba1110c4a832637
Gilang <maulana.gilang305@gmail.com>
2024-08-03 17:14:06 +0700

    use vrf2plusclient

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -8,28 +8,56 @@ pragma solidity ^0.8.19;
  * @dev    Implement Chainlink VRF version 2.5
  */
 
-contract Raffle{
+import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+
+contract Raffle is VRFConsumerBaseV2Plus{
     error Raffle__sendMoreEth();
 
-    uint256 private immutable i_entranceFee; 
+    uint256 private immutable i_entranceFee;
+    uint256 private immutable i_interval;
     address payable[] private s_players;
+    uint256 private s_lastTimeStamp;
 
-    constructor(uint256 entranceFee){
+    event RaffleEnter(address indexed player);
+
+    constructor(uint256 entranceFee, uint256 interval, address vrfCoordinator) VRFConsumerBaseV2Plus(vrfCoordinator) {
         i_entranceFee = entranceFee;
+        i_interval = interval;
+        s_lastTimeStamp = block.timestamp;
     }
 
-    function enterRaffle() public payable{
-        if(msg.value < i_entranceFee){
+    function enterRaffle() external payable {
+        if (msg.value < i_entranceFee) {
             revert Raffle__sendMoreEth();
         }
         s_players.push(payable(msg.sender));
+        emit RaffleEnter(msg.sender);
     }
 
-    function pickWinner() public{
+    function pickWinner() external {
+        if ((block.timestamp - s_lastTimeStamp) > i_interval) {
+            revert();
+        }
+        // requestId = s_vrfCoordinator.requestRandomWords(
+        //     VRFV2PlusClient.RandomWordsRequest({
+        //         keyHash: s_keyHash,
+        //         subId: s_subscriptionId,
+        //         requestConfirmations: requestConfirmations,
+        //         callbackGasLimit: callbackGasLimit,
+        //         numWords: numWords,
+        //         extraArgs: VRFV2PlusClient._argsToBytes(
+        //             // Set nativePayment to true to pay for VRF requests with Sepolia ETH instead of LINK
+        //             VRFV2PlusClient.ExtraArgsV1({nativePayment: false})
+        //         )
+        //     })
+        // );
+    }
+
+    function fulfillRandomWords(uint256 requetId, uint256[] calldata randomWords) internal override {
 
     }
 
-    function getEntranceFee() external view returns(uint256){
+    function getEntranceFee() external view returns (uint256) {
         return i_entranceFee;
     }
 }

7ac5e50d47bbfcf0447444e1d87a3be0ea635d5d
Gilang <maulana.gilang305@gmail.com>
2024-08-03 17:14:27 +0700

    little adjustment

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -9,6 +9,7 @@ pragma solidity ^0.8.19;
  */
 
 import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
 
 contract Raffle is VRFConsumerBaseV2Plus{
     error Raffle__sendMoreEth();
@@ -17,13 +18,21 @@ contract Raffle is VRFConsumerBaseV2Plus{
     uint256 private immutable i_interval;
     address payable[] private s_players;
     uint256 private s_lastTimeStamp;
+    bytes32 private immutable i_keyHash;
+    uint256 private immutable i_subscriptionId;
+    uint16 private constant REQUEST_CONFIRMATION = 3;
+    uint32 private immutable i_callbackGasLimit;
+    uint32 private constant NUM_WORDS = 1;
 
     event RaffleEnter(address indexed player);
 
-    constructor(uint256 entranceFee, uint256 interval, address vrfCoordinator) VRFConsumerBaseV2Plus(vrfCoordinator) {
+    constructor(uint256 entranceFee, uint256 interval, address vrfCoordinator, bytes32 gasLane, uint256 subscriptionId, uint32 callbackGasLimit) VRFConsumerBaseV2Plus(vrfCoordinator) {
         i_entranceFee = entranceFee;
         i_interval = interval;
         s_lastTimeStamp = block.timestamp;
+        i_keyHash = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
     }
 
     function enterRaffle() external payable {
@@ -38,7 +47,21 @@ contract Raffle is VRFConsumerBaseV2Plus{
         if ((block.timestamp - s_lastTimeStamp) > i_interval) {
             revert();
         }
-        // requestId = s_vrfCoordinator.requestRandomWords(
+
+        VRFV2PlusClient.RandomWordsRequest memory request = VRFV2PlusClient.RandomWordsRequest({
+            keyHash: i_keyHash,
+            subId: i_subscriptionId,
+            requestConfirmations: REQUEST_CONFIRMATION,
+            callbackGasLimit: i_callbackGasLimit,
+            numWords: NUM_WORDS,
+            extraArgs: VRFV2PlusClient._argsToBytes(
+                VRFV2PlusClient.ExtraArgsV1({nativePayment: false})
+            )
+        });
+
+        uint256 requestId = s_vrfCoordinator.requestRandomWords(request);
+
+        // uint256 requestId = s_vrfCoordinator.requestRandomWords(
         //     VRFV2PlusClient.RandomWordsRequest({
         //         keyHash: s_keyHash,
         //         subId: s_subscriptionId,
